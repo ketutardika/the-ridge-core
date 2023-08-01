18579a59f4cacc622bdc132b7b6dc4079bc79117
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-01 16:10:11 +0800

    Update the-ridge-widget-2.php

--- elementor/widgets/the-ridge-widget-2.php
+++ elementor/widgets/the-ridge-widget-2.php
@@ -60,32 +60,6 @@ class Elementor_The_Ridge_Widget_2 extends \Elementor\Widget_Base {
 			]
 		);
 
-		$this->end_controls_section();
-
-		// Content Tab End
-
-
-		// Style Tab Start
-
-		$this->start_controls_section(
-			'section_title_style',
-			[
-				'label' => esc_html__( 'Title', 'the-ridge-core' ),
-				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		$this->add_control(
-			'title_color',
-			[
-				'label' => esc_html__( 'Text Color', 'the-ridge-core' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .hello-world' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
 		$this->add_control(
 	        'cta_text',
 	        [
@@ -146,6 +120,32 @@ class Elementor_The_Ridge_Widget_2 extends \Elementor\Widget_Base {
 	            ],
 	        ]
 	    );
+
+		$this->end_controls_section();
+
+		// Content Tab End
+
+
+		// Style Tab Start
+
+		$this->start_controls_section(
+			'section_title_style',
+			[
+				'label' => esc_html__( 'Title', 'the-ridge-core' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'title_color',
+			[
+				'label' => esc_html__( 'Text Color', 'the-ridge-core' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .hello-world' => 'color: {{VALUE}};',
+				],
+			]
+		);
 
 		$this->end_controls_section();
 
