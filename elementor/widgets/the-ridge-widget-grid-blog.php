b7d30e14167b3775f76cb217d2123e988c5ec4cf
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-04 12:50:11 +0800

    Update the-ridge-widget-grid-blog.php

--- elementor/widgets/the-ridge-widget-grid-blog.php
+++ elementor/widgets/the-ridge-widget-grid-blog.php
@@ -194,7 +194,7 @@ class Elementor_The_Ridge_Widget_Grid_Blog extends \Elementor\Widget_Base {
     										$category_id = get_cat_ID( $categorys->name );
 							                ?>
 							                <h6 class="mt-3 display-9 section-heading-text text-color-primary">
-							                	<a href="<?php echo $category_id; ?>">
+							                	<a href="<?php echo esc_url( get_category_link($categorys->cat_ID) ); ?>">
 							                			<?php echo $categorys->name; ?>
 							                		</a>
 							                </h6>
