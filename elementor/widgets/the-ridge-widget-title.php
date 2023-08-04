20d1f5569c09a265ee198941671a4016ce624ffb
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-04 09:35:30 +0800

    update elementor widget core

--- elementor/widgets/the-ridge-widget-title.php
+++ elementor/widgets/the-ridge-widget-title.php
@@ -1,12 +1,12 @@
 <?php
-class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
+class Elementor_The_Ridge_Widget_Title extends \Elementor\Widget_Base {
 
 	public function get_name() {
-		return 'the_ridge_featured_cta';
+		return 'the_ridge_featured_title';
 	}
 
 	public function get_title() {
-		return esc_html__( 'The Ridge Featured CTA', 'the-ridge-core' );
+		return esc_html__( 'The Ridge Featured Title', 'the-ridge-core' );
 	}
 
 	public function get_icon() {
@@ -18,7 +18,7 @@ class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
 	}
 
 	public function get_keywords() {
-		return [ 'the ridge', 'featured cta' ];
+		return [ 'the ridge', 'featured title' ];
 	}
 
 	protected function register_controls() {
@@ -26,9 +26,9 @@ class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
 		// Content Tab Start
 
 		$this->start_controls_section(
-			'section_featured_cta',
+			'section_featured_title',
 			[
-				'label' => esc_html__( 'Featured CTA', 'the-ridge-core' ),
+				'label' => esc_html__( 'Featured Title', 'the-ridge-core' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
@@ -38,7 +38,16 @@ class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
 			[
 				'label' => esc_html__( 'Title', 'the-ridge-core' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Join our family', 'the-ridge-core' ),
+				'default' => esc_html__( 'Consectetur Adipiscing', 'the-ridge-core' ),
+			]
+		);
+
+		$this->add_control(
+			'subtitle',
+			[
+				'label' => esc_html__( 'Sub Title', 'the-ridge-core' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'The Villas' ),
 			]
 		);
 
@@ -48,7 +57,7 @@ class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
 			[
 				'label' => esc_html__( 'Description', 'the-ridge-core' ),
 				'type' => \Elementor\Controls_Manager::TEXTAREA,
-				'default' => esc_html__( 'Sign up now and embark on a journey of knowledge, inspiration, and exclusive benefits. Welcome to The Ridge family!', 'the-ridge-core' ),
+				'default' => esc_html__( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'the-ridge-core' ),
 			]
 		);
 
@@ -162,31 +171,23 @@ class Elementor_The_Ridge_Widget_Cta extends \Elementor\Widget_Base {
 		    }
 		?>
 
-		<!-- CTA Start -->
-        <div class="container-fluid-fluid px-0 ">
-            <div class="py-2 position-relative">
-                <div class="container-fluid my-5">
-                    <div class="row align-items-center">
-                        <div class="col-lg-3 col-md-7">
-                            <h5 class="display-6 h5 mb-0 text-white title-dark"><?php echo $settings['title']; ?></h5>
-                        </div><!--end col-->
-
-                        <div class="col-lg-5 col-md-7">
-                            <p class="display-8 mb-0 text-white title-dark"><?php echo $settings['description']; ?></p>
-                        </div><!--end col-->
-
-                        <div class="col-lg-4 col-md-5 text-md-center mt-4 mt-sm-0">
-                            <?php if ( $settings['cta_text'] && $cta_link ) : ?>
-						        <a class="btn btn-ridge-primary-reverse display-9 p-5 pt-2 pb-2 text-space-grotesk" href="<?php echo esc_url( $cta_link ); ?>">
-						            <?php echo esc_html( $settings['cta_text'] ); ?>
-						        </a>
-						    <?php endif; ?>
-                        </div><!--end col-->
-                    </div><!--end row-->
-                </div><!--end container-fluid-->
-            </div><!--end bg image-->
-        </div><!--end container-fluid-->
-        <!-- CTA End -->
+		<!-- Title Start -->
+		<div class="card">
+			<div class="card-body content">
+	            <h6 class="mt-3 display-9 section-heading-text text-color-primary"><?php echo $settings['subtitle']; ?></h6>
+	            <h4 class="display-6 mb-3 text-color-secondary"><?php echo $settings['title']; ?></h4>
+	            <p class="display-8 para-desc mx-auto mb-0 text-color-primary"><?php echo $settings['description']; ?>  
+	            </p>        
+	            <div class="mt-4">
+	                <?php if ( $settings['cta_text'] && $cta_link ) : ?>
+				        <a class="btn btn-ridge-primary-reverse display-9 p-5 pt-2 pb-2 text-space-grotesk" href="<?php echo esc_url( $cta_link ); ?>">
+				            <?php echo esc_html( $settings['cta_text'] ); ?>
+				        </a>
+				    <?php endif; ?>
+	            </div>
+	        </div>
+    	</div>
+    	<!-- Title End -->
 		<?php
 	}
 }
