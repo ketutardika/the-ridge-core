4f4d708246b0089119a2e1c6b3db57177e992a26
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-04 14:14:12 +0800

    Update the-ridge-widget-footer-list.php

--- elementor/widgets/the-ridge-widget-footer-list.php
+++ elementor/widgets/the-ridge-widget-footer-list.php
@@ -33,45 +33,60 @@ class Elementor_The_Ridge_Widget_Footer_List extends \Elementor\Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'footer_title',
+		$repeater = new \Elementor\Repeater();
+
+		$repeater->add_control(
+			'list_title',
 			[
-				'label' => esc_html__( 'Footer Title', 'the-ridge-core' ),
+				'label' => esc_html__( 'Title', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Information' ),
+				'default' => esc_html__( 'List Title' , 'textdomain' ),
+				'label_block' => true,
+			]
+		);
+
+		$repeater->add_control(
+			'list_content',
+			[
+				'label' => esc_html__( 'Content', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::WYSIWYG,
+				'default' => esc_html__( 'List Content' , 'textdomain' ),
+				'show_label' => false,
+			]
+		);
+
+		$repeater->add_control(
+			'list_color',
+			[
+				'label' => esc_html__( 'Color', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} {{CURRENT_ITEM}}' => 'color: {{VALUE}}'
+				],
 			]
 		);
 
 		$this->add_control(
 			'list',
 			[
-				'label' => esc_html__( 'Footer List', 'the-ridge-core' ),
+				'label' => esc_html__( 'Repeater List', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::REPEATER,
-				'field' => [
-					[
-						'name' => 'list_title',
-						'label' => esc_html__( 'Title', 'textdomain' ),
-						'type' => \Elementor\Controls_Manager::TEXT,
-						'default' => esc_html__( 'List Title' , 'textdomain' ),
-						'label_block' => true,
-					],
-				],
+				'fields' => $repeater->get_controls(),
 				'default' => [
 					[
 						'list_title' => esc_html__( 'Title #1', 'textdomain' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'textdomain' ),
 					],
 					[
 						'list_title' => esc_html__( 'Title #2', 'textdomain' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'textdomain' ),
 					],
 				],
 				'title_field' => '{{{ list_title }}}',
 			]
 		);
 
-
 		$this->end_controls_section();
-
-		// Content Tab End
 
 	}
 
@@ -82,8 +97,22 @@ class Elementor_The_Ridge_Widget_Footer_List extends \Elementor\Widget_Base {
 			echo '<dl>';
 			foreach (  $settings['list'] as $item ) {
 				echo '<dt class="elementor-repeater-item-' . esc_attr( $item['_id'] ) . '">' . $item['list_title'] . '</dt>';
+				echo '<dd>' . $item['list_content'] . '</dd>';
 			}
 			echo '</dl>';
 		}
+	}
+
+	protected function content_template() {
+		?>
+		<# if ( settings.list.length ) { #>
+			<dl>
+			<# _.each( settings.list, function( item ) { #>
+				<dt class="elementor-repeater-item-{{ item._id }}">{{{ item.list_title }}}</dt>
+				<dd>{{{ item.list_content }}}</dd>
+			<# }); #>
+			</dl>
+		<# } #>
+		<?php
 	}
 }
